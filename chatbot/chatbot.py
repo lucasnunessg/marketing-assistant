2f32c803ec1f3e3bff0c24d78ef97a3eb8126b78
lucasnunessg <lucasnunespacheco@gmail.com>
2025-05-03 09:32:04 -0300

    feat: chatbot sem guardrails ainda

---
+++ chatbot/chatbot.py
@@ -0,0 +1,22 @@
+from langchain_ollama import ChatOllama
+from ..prompt import marketing_prompt
+
+llm = ChatOllama(
+    model="gemma:2b",
+    temperature=0.2,  
+    num_ctx=1024,
+    num_thread=4
+)
+
+def ask_llm(question: str, docs: list[str]) -> str:
+  context= "\n".join(docs)
+  prompt = marketing_prompt.format(question=question, context=context)
+
+  print("Prompt enviado ao LLM! ", prompt)
+
+  try:
+    response = llm.invoke(prompt)
+    return response.content
+  except Exception as e:
+    print(f"Erro completo: {str(e)}")
+    return f"Erro ao processar: {str(e)}"
